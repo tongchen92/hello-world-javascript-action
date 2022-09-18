6799e052a5d639d1b8d2ee58cc48bc0f158a7165
Tong Chen <tong.chen@roblox.com>
2022-09-17 17:58:00 -0700

    fix

--- index.js
+++ index.js
@@ -8,19 +8,17 @@ try {
 //   const time = (new Date()).toTimeString();
 //   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-  const fs = require('fs');
+//   const payload = JSON.stringify(github.context.payload, undefined, 2)
 
-  let student = { 
-      name: 'Mike',
-      age: 23, 
-      gender: 'Male',
-      department: 'English',
-      car: 'Honda' 
-  };
+//   let student = { 
+//       name: 'Mike',
+//       age: 23, 
+//       gender: 'Male',
+//       department: 'English',
+//       car: 'Honda' 
+//   };
    
-  let data = JSON.stringify(student);
+//   let data = JSON.stringify(student);
   core.setOutput("test", "test data");
 } catch (error) {
   core.setFailed(error.message);

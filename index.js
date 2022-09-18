10d2548a4da9d41ef664de47789b2000dbcf93e8
Tong Chen <tong.chen@roblox.com>
2022-09-17 17:17:06 -0700

    fix

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+  const fs = require('fs');
+
+  let student = { 
+      name: 'Mike',
+      age: 23, 
+      gender: 'Male',
+      department: 'English',
+      car: 'Honda' 
+  };
+   
+  let data = JSON.stringify(student);
+  fs.writeFileSync('student-2.json', data);
 } catch (error) {
   core.setFailed(error.message);
 }

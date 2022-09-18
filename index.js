668199ef105ac3ca76f185cdf62a8f20678a7dc1
Tong Chen <tong.chen@roblox.com>
2022-09-17 18:01:44 -0700

    fix

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ try {
 //   const nameToGreet = core.getInput('who-to-greet');
 //   console.log(`Hello ${nameToGreet}!`);
 //   const time = (new Date()).toTimeString();
-//   core.setOutput("time", time);
+  core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
 //   const payload = JSON.stringify(github.context.payload, undefined, 2)
 
@@ -19,7 +19,7 @@ try {
 //   };
    
 //   let data = JSON.stringify(student);
-  core.setOutput("test", "test data");
+//   core.setOutput("test", "test data");
 } catch (error) {
   core.setFailed(error.message);
 }
